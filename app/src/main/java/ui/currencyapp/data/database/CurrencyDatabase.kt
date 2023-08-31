3eb210fe66cf80e189926a6d75a7f2e3eb1910bf
mohamedelgammal <mohamedelgammal2244@gmail.com>
2023-08-31 09:43:08 +0300

    Fix Database Issue

--- app/src/main/java/ui/currencyapp/data/database/CurrencyDatabase.kt
+++ app/src/main/java/ui/currencyapp/data/database/CurrencyDatabase.kt
@@ -4,8 +4,6 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
-import ui.currencyapp.data.database.CurrencyDAO
-import ui.currencyapp.data.database.RateDAO
 
 @Database(entities = [Currency::class, Rate::class], version = 1, exportSchema = false)
 abstract class CurrencyDatabase : RoomDatabase() {
